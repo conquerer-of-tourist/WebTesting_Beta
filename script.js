0ab669140a5258da61b1efe87d1421b8de556f66
Sophon Proton Flying Boy <67530900+conquerer-of-tourist@users.noreply.github.com>
2024-08-21 10:48:18 -0700

    Update script.js retry commit for pp changes

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const heading = document.getElementById("myHeading");
 const text = heading.textContent;
 const newText = text.replace(/\./g, '<span class="purple-dot">.</span>')
 heading.innerHTML = newText;
+
